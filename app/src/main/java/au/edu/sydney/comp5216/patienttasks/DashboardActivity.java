7bbc8abc59f665e7f78b7b390ea1234f1fc603fc
Dennis <zyl1251994@gmail.com>
2020-11-05 21:16:37 +1100

    Modified firebase Auth

---
+++ app/src/main/java/au/edu/sydney/comp5216/patienttasks/DashboardActivity.java
@@ -0,0 +1,13 @@
+package au.edu.sydney.comp5216.patienttasks;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
