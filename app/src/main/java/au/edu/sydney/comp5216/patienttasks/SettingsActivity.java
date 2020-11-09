df78134a527bb3781644ba84d1fab19f6740c3a9
Dennis <zyl1251994@gmail.com>
2020-11-09 18:46:39 +1100

    added sign out button1

--- app/src/main/java/au/edu/sydney/comp5216/patienttasks/SettingsActivity.java
+++ app/src/main/java/au/edu/sydney/comp5216/patienttasks/SettingsActivity.java
@@ -2,12 +2,31 @@ package au.edu.sydney.comp5216.patienttasks;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SettingsActivity extends AppCompatActivity {
+    private Button signOutBtn;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        mAuth = FirebaseAuth.getInstance();
+        signOutBtn = findViewById(R.id.sign_out);
+
+        signOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                Intent intent = new Intent(SettingsActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
